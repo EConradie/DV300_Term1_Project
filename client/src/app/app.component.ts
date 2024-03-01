b56ce53e74c2bdc7b1ac2fdb2ced865ed43618d6
Erik Conradie <113360820+EConradie@users.noreply.github.com>
2024-03-01 16:06:46 +0200

    Navbar flexbox fix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './features/components/nav-bar/nav-bar.component'; 
+import { LandingPageComponent } from './features/pages/landing-page/landing-page.component'; 
+import { PackagesPageComponent } from './features/pages/packages-page/packages-page.component';
+import { CustomPackageComponent } from './features/pages/custom-package/custom-package.component';
+import { WarehouseComponent } from './features/pages/warehouse/warehouse.component';
+import { LoginPageComponent } from './features/pages/auth/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, NavBarComponent, LandingPageComponent, PackagesPageComponent, CustomPackageComponent, WarehouseComponent, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/src/app/features/pages/packages-page/packages-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-packages-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './packages-page.component.html',
+  styleUrl: './packages-page.component.css'
+})
+export class PackagesPageComponent {
+
+}
