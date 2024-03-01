d4abc1eebd72a0a0615b64ab32c49dfe415e7dae
Erik Conradie <113360820+EConradie@users.noreply.github.com>
2024-03-01 10:50:39 +0200

    Server Setup / Temp

---
+++ server/src/entity/lecturer.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+
+
+@Entity()
+export class Lecturer {
+    @PrimaryGeneratedColumn()
+    id: number = 0;
+
+    @Column({length: 225})
+    username: string = 'placeholder'
+
+    @Column({length: 225})
+    email: string = 'email@email.com'
+
+    @Column()
+    years: number = 1
+
+}
