7b8125603f5977f7150b0130b3ad37782786d137
Erik Conradie <113360820+EConradie@users.noreply.github.com>
2024-03-01 10:47:06 +0200

    Setup

---
+++ client/src/app/features/models/item-model/item-model.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-model',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-model.component.html',
+  styleUrl: './item-model.component.css'
+})
+export class ItemModelComponent {
+
+}
