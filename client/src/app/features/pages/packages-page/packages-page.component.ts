b6b40f8f69c0685f240e77f9619f16938bce1e43
EmilioCarreiraOWI <113447065+EmilioCarreiraOWI@users.noreply.github.com>
2024-03-10 09:37:56 +0200

    landing page UI in progress

--- client/src/app/features/pages/packages-page/packages-page.component.ts
+++ client/src/app/features/pages/packages-page/packages-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { StockQuantityComponent } from '../../components/stock-quantity/stock-quantity.component';
 
 @Component({
   selector: 'app-packages-page',
   standalone: true,
-  imports: [],
+  imports: [StockQuantityComponent],
   templateUrl: './packages-page.component.html',
   styleUrl: './packages-page.component.css'
 })
